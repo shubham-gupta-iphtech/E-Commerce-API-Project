19fad6d0f6d5a28156a6275c3e1138bf4dfaaf02
Shubham Gupta <>
2025-04-04 14:39:15 +0530

    added user validation code

---
+++ middleware/authValidation.js
@@ -0,0 +1,17 @@
+import { body } from "express-validator"
+    
+const registerValidation = 
+[
+    body('email').isEmail().withMessage('Invalid email'),
+    body('name').notEmpty().withMessage('Please provide name'),
+    body('password').isLength({ min: 6 }).withMessage('Password too short'),
+    body('role').isIn(['admin'],['customer']).withMessage('invalid role. Must be an admin or customer'),
+]   
+
+const loginValidation = [
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').notEmpty().withMessage('Password is required'),
+  ];
+   
+
+export {registerValidation, loginValidation};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { registerUser, loginUser } from '../controllers/authControllers.js';
+import validate from '../middleware/validate.js'; 
+import {registerValidation, loginValidation} from '../middleware/authValidation.js'
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-
+router.post('/register', registerValidation, validate, registerUser);
+router.post('/login', loginValidation, validate, loginUser);
+    
 export default router;
